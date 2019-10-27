ec56928622e00e56d46233c1fdac23588b55219f
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2019-10-27 18:46:41 +0900

    add add all restaurants table

--- controllers/restaurants/index.js
+++ controllers/restaurants/index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 
 module.exports = models => {
-  const listRestaurant = (req, res) => {
-    return (
-      models.restaurants
-        .list()
-        //   .then(restaurants => {
-        //     restaurants.map(restaurant => restaurant);
-        //   })
-        .then(restaurants => res.status(200).json(restaurants))
-        .catch(err => res.status(400).send(err.message))
-    );
+  const listRestaurants = (req, res) => {
+    return models.restaurants
+      .list()
+      .then(restaurants => res.status(200).json(restaurants))
+      .catch(err => res.status(400).send(err.message));
+  };
+
+  const addAllRestaurants = (req, res) => {
+    return models.restaurants
+      .create()
+      .then(restaurants => res.status(200).json(restaurants))
+      .catch(err => res.status(400).send(err.message));
   };
 
   const router = express.Router();
-  router.get("/", listRestaurant);
+  router.get("/", listRestaurants);
+  router.get("/add", addAllRestaurants);
 
   return router;
 };
--- models/restaurants/create.js
+++ models/restaurants/create.js
@@ -1,36 +1,38 @@
-//TODO: later
-// const axios = require("axios");
+const axios = require("axios");
 
-// const endPoint =
-//   "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=44d836440e0d4ee946d125292730a20c&&latitude=35.658062&longitude=139.7275625&range=5";
+const endPoint =
+  "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=44d836440e0d4ee946d125292730a20c&&latitude=35.658062&longitude=139.7275625&range=5";
 
-// const restaurants = axios
-//   .get(endPoint)
-//   .then(response => {
-//     return response.data.rest.map(restaurant => {
-//       return [
-//         restaurant.name,
-//         restaurant.name_kana,
-//         restaurant.category,
-//         restaurant.url,
-//         restaurant.address,
-//         restaurant.tel,
-//         restaurant.opentime,
-//         restaurant.budget,
-//         restaurant.lunch,
-//         restaurant.credit_card
-//       ];
-//     });
-//   })
-//   .catch(error => {
-//     console.error("network error", error);
-//   });
+const restaurants = axios
+  .get(endPoint)
+  .then(response => {
+    return response.data.rest.map(restaurant => {
+      return {
+        name: restaurant.name,
+        name_kana: restaurant.name_kana,
+        category: restaurant.category,
+        url: restaurant.url,
+        address: restaurant.address,
+        tel: restaurant.tel,
+        opentime: restaurant.opentime,
+        budget: restaurant.budget,
+        lunch: restaurant.lunch,
+        credit_card: restaurant.credit_card
+      };
+    });
+  })
+  .catch(error => {
+    console.error("network error", error);
+  });
 
-// module.exports = knex => {
-//   return () => {
-//     console.log("a");
-//     return knex("restaurants")
-//       .insert(restaurants)
-//       .catch(err => new Error(`Error ${err}`));
-//   };
-// };
+module.exports = knex => {
+  return () => {
+    if (restaurants)
+      return restaurants.then(res => {
+        return knex("restaurants")
+          .insert(res)
+          .catch(err => new Error(`Error ${err}`));
+      });
+    return { statuscode: 303 };
+  };
+};
