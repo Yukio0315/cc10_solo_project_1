afc69242bb5092a7a1920357b6ec42b7e2d43cf2
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2019-10-27 23:12:08 +0900

    change list range

--- models/restaurants/list.js
+++ models/restaurants/list.js
@@ -1,6 +1,8 @@
 module.exports = knex => {
   return () => {
     return knex("restaurants")
+      .where("deleted", null)
+      .orWhere("deleted", false)
       .select()
       .catch(err => new Error(`Error ${err}`));
   };
